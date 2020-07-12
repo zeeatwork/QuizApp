8c2ba4c46ad9427511fa7ff9666321d01adbd103
Zenzi Ali <zeeathome935@gmail.com>
2020-07-12 15:40:30 -0400

    Added CSS Elements

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container');
+let shuffledQuestions;
+let currentQuestionIndex;
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+
+startButton.addEventListener('click', startGame);
+
+function startGame(){
+  console.log('Started');
+  startButton.classList.add('hide');
+  shuffledQestions = questions.sort(() => Math.random() - .5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+ showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function sshowQuestion(question) {
+  questionElement.innerText = question.question
+}
+
+
+
+function selectAnswer() {
+
+
+}
+
+const quizQuestions = [
+  {
+    question: '',
+    answers: [
+      {}
+      {}
+      {}
+      {}
+    ]
+  }
+]
